1e9dcdb97ec3e33180fc2b0645df4da4742adb83
Ben Hockley <ben.hockley@readify.net>
2019-08-01 14:10:31 +1000

    Initial webpack build

---
+++ AspAndWebpack/webpack.config.js
@@ -0,0 +1,10 @@
+﻿var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './scripts/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
